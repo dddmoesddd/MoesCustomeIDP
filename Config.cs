6937b1d6116155b20576ba13f461b08cb6e9c560
Your Name <you@example.com>
2022-09-07 10:16:02 +0430

    first  commit after securti concern

---
+++ Config.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace MoesCustomIDP
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                 new IdentityResources.Profile(),
+                            new IdentityResources.Address(),
+                            new IdentityResource("roles","Your Role",new List<string>(){"role" })
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            { };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[] 
+            { 
+              new Client {
+                  ClientName="AccountTransactiom" ,
+                  RequirePkce=true,
+                  ClientId="accounttransactionclient",
+                  AllowedGrantTypes=GrantTypes.Code,
+                  RedirectUris =new List<string>{
+
+                      "https://localhost:44372/signin-oidc"
+                  },
+                               PostLogoutRedirectUris = new List<string>()
+                    {
+                        "https://localhost:44372/signout-callback-oidc"
+                    },
+                  AllowedScopes =
+                  {
+
+                     IdentityServer4.IdentityServerConstants.StandardScopes.OpenId,
+                           IdentityServer4.IdentityServerConstants.StandardScopes.Profile,
+                            IdentityServer4.IdentityServerConstants.StandardScopes.Address,
+                                     "roles"
+                  },
+
+                  ClientSecrets =
+                  {
+                      new Secret("secret".Sha256())
+
+                  }
+              }
+            };
+    }
+}
