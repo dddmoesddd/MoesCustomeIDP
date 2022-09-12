5d7fd7ff51c6acbe19f3df99bcf4939296081874
Mohammad Eslahi <moesgm363@gmail.com>
2022-09-12 22:55:58 +0430

    add persistance congiguration

--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,16 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServerHost.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Reflection;
 
 namespace MoesCustomIDP
 {
@@ -24,14 +29,37 @@ namespace MoesCustomIDP
             // uncomment, if you want to add an MVC-based UI
             services.AddControllersWithViews();
 
+
+
             var builder = services.AddIdentityServer(options =>
             {
                 // see https://identityserver4.readthedocs.io/en/latest/topics/resources.html
                 options.EmitStaticAudienceClaim = true;
             })
-                .AddInMemoryIdentityResources(Config.IdentityResources).
-                AddInMemoryApiResources(Config.ApiResources).AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryClients(Config.Clients).AddTestUsers(TestUsers.Users);
+                //.AddInMemoryIdentityResources(Config.IdentityResources).
+                //AddInMemoryApiResources(Config.ApiResources).AddInMemoryApiScopes(Config.ApiScopes)
+                //.AddInMemoryClients(Config.Clients).
+                .AddTestUsers(TestUsers.Users);
+
+            var MoesIDPPDataDBConnectionString =
+"Server=DESKTOP-2UNIFB0;Database=MoesIDPDataDB;Trusted_Connection=True;";
+            var migrationsAssembly = typeof(Startup)
+.GetTypeInfo().Assembly.GetName().Name;
+            builder.AddOperationalStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(MoesIDPPDataDBConnectionString,
+                    options => options.MigrationsAssembly(migrationsAssembly));
+            });
+
+
+            builder.AddConfigurationStore(options =>
+            {
+                options.ConfigureDbContext = builder =>
+                    builder.UseSqlServer(MoesIDPPDataDBConnectionString,
+                    options => options.MigrationsAssembly(migrationsAssembly));
+            });
+
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
@@ -44,10 +72,11 @@ namespace MoesCustomIDP
                 app.UseDeveloperExceptionPage();
             }
 
+            InitializeDatabase(app);
             // uncomment if you want to add MVC
             app.UseStaticFiles();
             app.UseRouting();
-            
+
             app.UseIdentityServer();
 
             // uncomment, if you want to add MVC
@@ -56,6 +85,55 @@ namespace MoesCustomIDP
             {
                 endpoints.MapDefaultControllerRoute();
             });
+        }
+
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                .GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider
+                    .GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider
+                    .GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.Clients)
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.IdentityResources)
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.ApiResources)
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+                if (!context.ApiScopes.Any())
+                {
+                    foreach (var resource in Config.ApiScopes)
+                    {
+                        context.ApiScopes.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
